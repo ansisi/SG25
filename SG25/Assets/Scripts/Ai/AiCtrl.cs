3d76af89efd02a7e7bc20686ecbd4e43e0885775
jintg0821 <jintg0825@gmail.com>
2024-05-16 08:04:50 +0900

    Feat "진열대마다 랜덤으로 아이템 가져오기 구현"

--- SG25/Assets/Scripts/Ai/AiCtrl.cs
+++ SG25/Assets/Scripts/Ai/AiCtrl.cs
@@ -1,33 +1,41 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AiCtrl : MonoBehaviour
 {
-    private NavMeshAgent agent;
-    public Transform[] waypoints;
-    private int currentWaypointIndex = 0;
-    public float movementSpeed = 50f;
-
     public Transform itemHoldPoint;
     public Transform itemDropPoint;
 
     public LayerMask itemLayer;
-    public float itemSearchRange = 2f;
-    public float itemPickRange = 2f;
+    public Collider agentCollider;
 
-    private List<GameObject> searchItems = new List<GameObject>();
-    private List<GameObject> selectedObjects = new List<GameObject>();
     public MoneyConsumable[] moneyPrefabs;
+    public Transform[] waypoints;
 
+    private NavMeshAgent agent;
+    private Transform currentShelfInRange;
+    //private Animator animator;
+
+    public float movementSpeed = 20f;
+    private int currentWaypointIndex = 0;
+
+    private bool isColliderEnabled = false;
+
+    private List<Transform> checkedShelves = new List<Transform>();
+    private List<Transform> heldItems = new List<Transform>();
     private List<GameObject> holdItems = new List<GameObject>();
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        //animator = GetComponent<Animator>();
         agent.speed = movementSpeed;
+        currentWaypointIndex = 0;
+        agentCollider.enabled = false;
 
-        SearchItem();
+        MoveToNextWaypoint();
     }
 
     void MoveToNextWaypoint()
@@ -35,133 +43,131 @@ public class AiCtrl : MonoBehaviour
         if (currentWaypointIndex < waypoints.Length)
         {
             agent.SetDestination(waypoints[currentWaypointIndex].position);
+            //animator.SetBool("isWalking", true);
             Debug.Log("다음 웨이포인트로 이동 중: " + waypoints[currentWaypointIndex].name);
-            currentWaypointIndex++;
+            Debug.Log("현재 목적지: " + waypoints[currentWaypointIndex].position);
+        }
+    }
 
-            foreach (GameObject obj in selectedObjects)
+    IEnumerator WaitForOneSecond()
+    {
+        agentCollider.enabled = true;
+        isColliderEnabled = true;
+        //animator.SetBool("isWalking", true);
+        GetItems();
+
+        yield return new WaitForSeconds(0.1f);
+
+        agentCollider.enabled = false;
+        isColliderEnabled = false;
+        //animator.SetBool("isWalking", false);
+
+        currentWaypointIndex++;
+        MoveToNextWaypoint();
+    }
+
+
+
+    private List<Transform> SearchItems(Transform shelf, int itemCount)
+    {
+        List<Transform> targetItems = new List<Transform>();
+        Transform[] slots = shelf.GetComponentsInChildren<Transform>();
+
+        List<Transform> itemSlots = new List<Transform>();
+        foreach (Transform slot in slots)
+        {
+            if (slot.CompareTag("Slot") && slot.childCount > 0)
             {
-                if ((obj != null))
+                itemSlots.Add(slot);
+            }
+        }
+
+        if (itemSlots.Count > 0)
+        {
+            List<int> indices = new List<int>();
+            for (int i = 0; i < Mathf.Min(itemCount, itemSlots.Count); i++)
+            {
+                int randomIndex = Random.Range(0, itemSlots.Count);
+                while (indices.Contains(randomIndex))
                 {
-                    if (itemPickRange <= 4)
-                    {
-                        agent.SetDestination(obj.transform.position);
-                    }
+                    randomIndex = Random.Range(0, itemSlots.Count);
                 }
+                indices.Add(randomIndex);
+                Transform slotWithItem = itemSlots[randomIndex];
+                Transform item = slotWithItem.GetChild(0);
+                targetItems.Add(item);
             }
         }
         else
         {
-            agent.isStopped = true;
-            DropItem();
-            return;
+            currentWaypointIndex++;
+            Debug.Log("No items found, moving to the next waypoint.");
+        }
+        return targetItems;
+    }
+
+    private void GetItems()
+    {
+        int itemCount = 3;
+
+        if (currentShelfInRange != null)
+        {
+            List<Transform> targetItems = SearchItems(currentShelfInRange, itemCount);
+            if (targetItems.Count > 0)
+            {
+                foreach (Transform item in targetItems)
+                {
+                    StartCoroutine(MoveItemToHoldPoint(item));
+                }
+            }
+            else
+            {
+                MoveToNextWaypoint();
+
+            }
         }
     }
-    void SearchItem()
+
+    private IEnumerator MoveItemToHoldPoint(Transform item)
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, itemSearchRange, itemLayer);
-        searchItems.Clear();
+        //animator.SetBool("isPicking", true);
 
-        foreach (Collider collider in colliders)
-        {
-            searchItems.Add(collider.gameObject);
-        }
+        item.SetParent(itemHoldPoint);
+        item.localPosition = Vector3.zero;
+        item.localRotation = Quaternion.identity;
 
-        int itemsCount = Mathf.Min(Random.Range(0, 30), searchItems.Count);
+        heldItems.Add(item);
 
-        for (int i = 0; i < itemsCount; i++)
-        {
-            int randomIndex = Random.Range(0, searchItems.Count);
+        yield return new WaitForSeconds(0.1f);
 
-            selectedObjects.Add(searchItems[randomIndex]);
-            searchItems.RemoveAt(randomIndex);
-        }
-        Debug.Log(selectedObjects.Count);
-        MoveToNextWaypoint();
+        //animator.SetBool("isPicking", false);
     }
 
     void DropItem()
     {
-        int totalValue = 0;
 
-        foreach (GameObject itemObj in holdItems)
-        {
-            Consumable consumable = itemObj.GetComponent<Consumable>();
-            if (consumable.item != null)
-            {
-                totalValue += consumable.item.price;
-            }
-        }
-
-        Debug.Log("총 가치: " + totalValue);
-
-        foreach (MoneyConsumable moneyConsumable in moneyPrefabs)
-        {
-            int moneyValue = moneyConsumable.money.value;
-
-            if (totalValue >= moneyValue)
-            {
-                int moneyToGive = Random.Range(moneyValue, totalValue + 1);
-
-                if (moneyToGive < moneyValue)
-                {
-                    moneyToGive = moneyValue;
-                }
-
-                GameObject moneyObject = Instantiate(moneyConsumable.money.moneyPrefab, itemHoldPoint.position, itemHoldPoint.rotation);
-                MoneyConsumable moneyComponent = moneyObject.GetComponent<MoneyConsumable>();
-
-
-                if (moneyComponent != null)
-                {
-                    moneyComponent.money.value = moneyValue;
-
-                    moneyObject.transform.SetParent(itemHoldPoint);
-                    moneyObject.transform.localPosition = Vector3.zero;
-                    moneyObject.transform.localRotation = Quaternion.identity;
-                }
-                totalValue -= moneyValue;
-                Debug.Log("주는 돈의 가치: " + moneyToGive);
-            }
-            foreach (GameObject itemObj in holdItems)
-            {
-                itemObj.transform.SetParent(itemDropPoint);
-                itemObj.transform.position = itemDropPoint.position;
-                itemObj.transform.rotation = itemDropPoint.rotation;
-                itemObj.tag = "Product";
-            }
-
-            holdItems.Clear();
-            agent.isStopped = true;
-        }
-    }
-
-    void Update()
-    {
-        if (!agent.pathPending && agent.remainingDistance < 0.1f)
-        {
-            MoveToNextWaypoint();
-        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (selectedObjects.Contains(other.gameObject))
-        {
-            if (other.CompareTag("Item"))
-            {
-                GameObject obj = other.gameObject;
-                float distance = Vector3.Distance(transform.position, obj.transform.position);
-                if (distance <= itemPickRange)
-                {
-                    obj.transform.SetParent(itemHoldPoint);
-                    obj.transform.localPosition = Vector3.zero;
-                    obj.transform.localRotation = Quaternion.identity;
+        Debug.Log(other.gameObject.name);
 
-                    holdItems.Add(obj);
-                    selectedObjects.Remove(obj);
-                    MoveToNextWaypoint();
-                }
+        if (other.CompareTag("Waypoint"))
+        {
+            if (agent.remainingDistance < agent.stoppingDistance && !agent.pathPending)
+            {
+                StartCoroutine(WaitForOneSecond());
+            }
+        }
+
+        if (other.CompareTag("Shelf"))
+        {
+            if (!checkedShelves.Contains(other.transform))
+            {
+                currentShelfInRange = other.transform;
+                checkedShelves.Add(other.transform);
             }
         }
     }
+
 }
