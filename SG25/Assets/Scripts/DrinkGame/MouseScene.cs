372308da635356867170e25df994c885542ca5ea
Hodawn <happygy925@naver.com>
2024-05-25 01:56:32 +0900

    Feat : "음료 미니게임 씬에 도착했을 때 마우스 커서 보이게"

--- SG25/Assets/Scripts/DrinkGame/MouseScene.cs
+++ SG25/Assets/Scripts/DrinkGame/MouseScene.cs
@@ -6,13 +6,8 @@ public class MouseScene : MonoBehaviour
 {
     void Start()
     {
-        // 게임이 시작될 때 마우스 커서를 보이게 합니다.
+        // DrinkMiniGameScene에서는 마우스 커서를 보이도록 설정
         Cursor.visible = true;
-    }
-
-    void OnDestroy()
-    {
-        // 게임 오브젝트가 제거될 때 마우스 커서를 다시 숨깁니다.
-        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.None; // 마우스 커서가 화면 내에서 자유롭게 움직일 수 있도록 설정
     }
 }
